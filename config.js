e4e312f493edd17455702ba6854edd6aaa834f9d
syam2007ser <148976317+syam2007ser@users.noreply.github.com>
2023-11-22 18:02:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['+919947121619', 'Syam-ser Official²⁰²³', true],
-  ['+919947121619', 'Syam-ser Official²⁰²³', true], 
+  ['+19709003557', 'Syam-ser Official²⁰²³', true],
+  ['+19709003557', 'Syam-ser Official²⁰²³', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['+919957121619'] 
-global.prems = ['+919947121619']
+global.mods = ['+19709003557'] 
+global.prems = ['+19709003557']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
